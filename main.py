ee34df8112913eebb5299cd5d57ab486e2b7eed1
Brian Nestan Nestan <43124953+zenoweed@users.noreply.github.com>
2022-03-23 06:56:49 +0000

    Converts words to ciphertext

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def caesar(pt, key):
   ascii_val = ord(pt)
   add_key = ascii_val + key
   cipher = chr(add_key)
-  print(cipher)
+  print(cipher,end="")
 
 k = 3
 p = 'ARKHAM'
@@ -12,9 +12,8 @@ p = 'ARKHAM'
 print ("Plaintext: ")
 print (p)
 print ("Cipertext: ")
-# caesar(p,k)
 
-i = 6 # number of letters in word
+i = len(p) # number of letters in word
 a=0
 while i != 0:
   letter = p[a]
