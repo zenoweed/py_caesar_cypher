7218ddbfb788140c900b23d5b4d601ed24549490
Brian Nestan Nestan <43124953+zenoweed@users.noreply.github.com>
2022-03-26 05:01:12 +0000

    decypher works 100% even after underflow

--- main.py
+++ main.py
@@ -1,56 +1,41 @@
 # Python program for caesar cypher
 
 k = 3
-p = 'THE QUICK FOX JUMPED OVER THE LAZY BROWN DOG'
+a = 0
+#p = 'THE QUICK FOX JUMPED OVER THE LAZY BROWN DOG'
+p = 'A B C X Y Z'
 i = len(p) # number of letters in word
-
-def enCaesar(pt, key):
-  if pt == " ":
-    print(pt, end="")
-  else:
-    ascii_val = ord(pt)
-    #print("ASCII VAL of "+ pt + ": ",ascii_val)
-    add_key = ascii_val + key
-    #print("ASCII VAL + key of "+ pt + ": ",add_key)
-    if add_key > 90:
-      mod = add_key - 90
-      add_key = 64 + mod
-      
-    cipher = chr(add_key)
-    print(cipher,end="")
 
 def deCaesar(pt, key):
   if pt == " ":
     print(pt, end="")
   else:
     ascii_val = ord(pt)
-    #print("ASCII VAL of "+ pt + ": ",ascii_val)
-    add_key = ascii_val + key
-    #print("ASCII VAL + key of "+ pt + ": ",add_key)
-    if add_key > 90:
-      mod = add_key - 90
-      add_key = 64 + mod
+    print("ASCII VAL of "+ pt + ": ",ascii_val)
+    add_key = ascii_val - key
+    print("ASCII VAL - key of "+ pt + ": ",add_key)
+    if add_key < 65:
+      mod =  65 - add_key
+      print(mod)
+      add_key = 91 - mod
       
     cipher = chr(add_key)
-    print(cipher,end="")
+    print("C: ")
+    print(cipher)#,end="")
 
 print ("Plaintext: ")
 print (p)
 
 
-choice = input("""  Choose:
-       1. Encipher
-       2. Decypher
-
-  Input: """)
+#choice = input("""  Choose:
+ ##     2. Decypher
+#
+ # Input: """) 
 
 print ("Cipertext: ",)
 while i != 0:
-  a = 0
   letter = p[a]
+  print("Letter: ",letter)
   a += 1
-  #if choice == 1:
-  enCaesar(letter,k)
-  #else:
-    #deCaesar(letter,k)
+  deCaesar(letter,k)
   i-=1
