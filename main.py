a1aa5b7bdeb4c2d54950f70da7321a957960fbe5
Brian Nestan Nestan <43124953+zenoweed@users.noreply.github.com>
2022-03-26 04:37:07 +0000

    Enciphering works 100% for capital overflowing.

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
 # Python program for caesar cypher
 
-def caesar(pt, key):
-  ascii_val = ord(pt)
-  add_key = ascii_val + key
-  cipher = chr(add_key)
-  print(cipher,end="")
-
 k = 3
-p = 'ARKHAM'
+p = 'THE QUICK FOX JUMPED OVER THE LAZY BROWN DOG'
+i = len(p) # number of letters in word
+
+def enCaesar(pt, key):
+  if pt == " ":
+    print(pt, end="")
+  else:
+    ascii_val = ord(pt)
+    #print("ASCII VAL of "+ pt + ": ",ascii_val)
+    add_key = ascii_val + key
+    #print("ASCII VAL + key of "+ pt + ": ",add_key)
+    if add_key > 90:
+      mod = add_key - 90
+      add_key = 64 + mod
+      
+    cipher = chr(add_key)
+    print(cipher,end="")
+
+def deCaesar(pt, key):
+  if pt == " ":
+    print(pt, end="")
+  else:
+    ascii_val = ord(pt)
+    #print("ASCII VAL of "+ pt + ": ",ascii_val)
+    add_key = ascii_val + key
+    #print("ASCII VAL + key of "+ pt + ": ",add_key)
+    if add_key > 90:
+      mod = add_key - 90
+      add_key = 64 + mod
+      
+    cipher = chr(add_key)
+    print(cipher,end="")
 
 print ("Plaintext: ")
 print (p)
-print ("Cipertext: ")
 
-i = len(p) # number of letters in word
-a=0
+
+choice = input("""  Choose:
+       1. Encipher
+       2. Decypher
+
+  Input: """)
+
+print ("Cipertext: ",)
 while i != 0:
+  a = 0
   letter = p[a]
-  a+=1
-  caesar(letter,k)
+  a += 1
+  #if choice == 1:
+  enCaesar(letter,k)
+  #else:
+    #deCaesar(letter,k)
   i-=1
