5dc0e87faafc01abf7145b555532d482f00d3801
Brian Nestan Nestan <43124953+zenoweed@users.noreply.github.com>
2022-03-27 05:23:33 +0000

    removed loop. works 100.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Python program for caesar cypher
 
-k = 3
-a = 0
-p = 'THE QUICK FOX JUMPED OVER THE LAZY BROWN DOG'
+k = 3 # key of cipher
+a = 0 # start of array
+p = 'THE QUICK FOX JUMPED OVER THE LAZY BROWN DOG' # plaintext
 i = len(p) # number of letters in word
+choice = 0 # choice of encryption or decryption
 
 def enCaesar(pt, key):
-  if pt == " ":
+  if pt == " ": # checks for spaces and returns em without any change
     print(pt, end="")
   else:
     ascii_val = ord(pt)
@@ -30,26 +31,27 @@ def deCaesar(pt, key):
       
     cipher = chr(add_key)
     print(cipher,end="")
-
-
-print ("Plaintext: ")
-print (p)
-l=1
-while l != 0:
-  choice = int(input("""\n\n  Choose:
-         1. Encipher
-         2. Decypher
   
-    Input: """))
-  
-  print ("Cipertext: ",)
-  while i != 0:
-    letter = p[a]
-    a += 1
-    if choice == 1:
-      enCaesar(letter,k)
-    elif choice == 2:
-      deCaesar(letter,k)
-    else:
-      print ("Invalid. Try again")
-    i-=1
+choice = int(input("""\n\n==========================================
+                   
+                   Choose:
+       1. Encipher
+       2. Decypher
+
+  Input: """))
+
+print ("  Choice: ", choice)
+print ("  Plaintext:",p,end="")
+print ("\n  Cipertext: ",end="")
+
+while a != i:
+  letter = p[a]
+  a += 1
+  if choice == 1:
+    enCaesar(letter,k)
+  elif choice == 2:
+    deCaesar(letter,k)
+  else:
+    print ("INVALID")
+
+print ("\n")
